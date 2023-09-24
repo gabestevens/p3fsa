744efdb733242a8de06a248a111ac9d8b7528f96
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-09-24 09:48:43 -0400

    user login controllers

--- my-app/backend/index.js
+++ my-app/backend/index.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const { Sequelize } = require('sequelize')
+// modules and globals
 require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
+// express settings
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// sequelize and postgres
+require('pg')
+const { Sequelize } = require('sequelize')
+
+// define port
 const PORT = process.env.PORT || 3001
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.DATABASE_URL)
-
-const app = express()
-
-app.use('/health', (req, res) => {
-    res.send('call to /health successful')
+// controllers and routes
+app.get('/', (req, res) => {
+    res.send('Welcome to Plenty of Flights backend API!')
 })
+
+app.use('/users', require('./controllers/users_controller'))
+app.use('/authentication', require('./controllers/authentication_controller'))
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
